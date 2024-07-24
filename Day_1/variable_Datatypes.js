8ea1a7c6512c5634f5531449ef27b54f06f61408
Shriya <strada929@rku.ac.in>
2024-07-24 19:18:55 +0530

    Initial commit

---
+++ Day_1/variable_Datatypes.js
@@ -0,0 +1,18 @@
+let a = null
+let b = 19
+let c = "Shriya"
+let d = BigInt("40568")
+let e = true
+let f ; // we can also write let f = undefined
+
+console.log(a , b , c , d , e , f )
+
+const student = {
+
+    name : "Shriya" ,
+    phonenumber : "98989 89898",
+    marks : "93%"
+}
+console.log(student["name"])
+console.log(student["phonenumber"])
+console.log(student["marks"])
